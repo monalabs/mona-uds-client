f23362cd602a974ecd5148e41a61651851fd11a0
Tzach Alon <tzach@monalabs.io>
2021-02-10 10:22:03 +0200

    Change Repo URL for PyPi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Client code for python Mona over Unix Domain Socket protocol",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/monalabs/mona-agent",
+    url="https://github.com/monalabs/mona-uds-client",
     download_url="http://pypi.python.org/pypi/mona-uds-client/",
     packages=setuptools.find_packages(),
     install_requires=["msgpack>=1.0.2"],
